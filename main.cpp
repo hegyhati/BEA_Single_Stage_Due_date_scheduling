3100005fb0dc405983c350e45dad7f66627c3eb5
Mate Hegyhati <hegyhati.work@gmail.com>
2017-11-05 23:31:53 +0100

    Command line arguments

--- main.cpp
+++ main.cpp
@@ -6,13 +6,19 @@
 #include "test/testBEA.hpp"
 
 #include<iostream>
-using namespace std;
+
 
 int main(int argc, char** argv){
-  if (argc==1) return 1;
-  else {
+  if (argc==1) {
+    std::cout<<"./a.out inputfile [localpoolsize] [population] [besteep] [new] [crosover]\n";
+  } else {
     Problem testProblem(argv[1]);
     printProblem(testProblem);
+    if(argc>2) Schedule::localPoolSize=atoi(argv[2]);
+    if(argc>3) BEA::populationSize=atoi(argv[3]);
+    if(argc>4) BEA::bestKeepCount=atoi(argv[4]);
+    if(argc>5) BEA::newCount=atoi(argv[5]);
+    if(argc>6) BEA::crossoverCount=atoi(argv[6]);
     testBEA(testProblem);
   }
 }
