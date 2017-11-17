24af882b9038b7541f65963e401906a9f5fbec62
Olivér Ősz <oliver.osz@ext.xbodyworld.com>
2017-11-17 22:51:03 +0100

    fixed input and added a new one too

--- test/testBEA.hpp
+++ test/testBEA.hpp
@@ -9,15 +9,22 @@
 
   void testBEA(const Problem* problem, int maxIterations){
     BEA test(problem,maxIterations);
+    double currentBest = -1;
     int generation=0;
     do{
-      cout<<setw(3)<<generation<<": (";
-      for(auto it: test.getCurrentValues())
-        cout<<it<<" ";
-      cout<<")\n"; 
-      generation++;     
-    } while (test.evolve()!=0 || generation < 100);
+      if (currentBest != test.getCurrentBestValue()) {
+        currentBest = test.getCurrentBestValue();
+        cout<<setw(4)<<generation<<": Found new best solution with value " << currentBest << endl;
+      } else if (generation%100 == 0) {
+        cout<<setw(4)<<generation<<": (";
+        for(auto it: test.getCurrentValues())
+          cout<<it<<" ";
+        cout<<")\n";
+      }
+      generation++;
+    } while (test.evolve()!=0 || generation < 3000);
     printSchedule(test.getCurrentBestSolution());
+    printMinEarlinessFinishTimes(test.getCurrentBestSolution());
   }
 
 #endif
--- test/testSchedule.hpp
+++ test/testSchedule.hpp
@@ -26,4 +26,33 @@
     cout<<"Objective value: "<<schedule.getObjectiveValue()<<"\n";
   }
 
+  void printMinEarlinessFinishTimes(Schedule schedule){
+    std::vector<std::vector<unsigned int>> prodlist(schedule.problem->getUnitCount());
+    for(unsigned int j=0;j<schedule.problem->getJobCount(); j++)
+      prodlist[(unsigned int) schedule.getPriority(j)].push_back(j);
+    for(unsigned int unit=0; unit<schedule.problem->getUnitCount(); unit++){
+      if (prodlist[unit].empty())
+        continue;
+      std::sort(prodlist[unit].begin(), prodlist[unit].end(), [&](unsigned int a, unsigned int b) {
+          return schedule.getPriority(a)<schedule.getPriority(b);
+      });
+      cout<<"U"<<unit+1<<":";
+      double finish = schedule.problem->getDeadline(prodlist[unit].back());
+      for (int j=prodlist[unit].size()-1; j>=0; --j){
+        double earliness=schedule.problem->getDeadline(prodlist[unit][j])-finish;
+        if (earliness < 0) {
+          finish+=earliness;
+          earliness=0;
+        }
+        double realFinish = finish;
+        finish-=schedule.problem->getProcTime(prodlist[unit][j],unit); //start time == latest finish time of next (previous in time)
+        printf(" i%d: %g-%g (%g);", prodlist[unit][j]+1, finish, realFinish, earliness);
+        if (j>0) {
+          finish-=schedule.problem->getSetupTime(prodlist[unit][j-1],prodlist[unit][j]);
+        }
+      }
+      cout<<endl;
+    }
+  }
+
 #endif
